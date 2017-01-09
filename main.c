1633c19fa53cbd388f348150ea3e0369d57da882
dcwang1224 <fnx1224@hotmail.com>
2017-01-09 11:43:09 +0800

    Update main.c
    origin/master 2 edit

--- main.c
+++ main.c
@@ -6,4 +6,6 @@ void main(char* argv[], int* argc)
       printf("Hello World\n");
       printf("origin/master add\n");
       printf("local master add\n");
+      
+      printf("origin/master test\n");
 }
